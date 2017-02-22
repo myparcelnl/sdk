14042f58d8764693c5844f043212f6ebda5f71d0
Leon Hagendijk <leon@logivert.com>
2017-02-22 17:25:22 +0100

    Fixes #6: Allow consignments without E-mail and partially addresses #7: Unable to run Unit Tests (fixes the Autoloader-part)

---
+++ Tests/SendConsignments/SendOneConsignmentWithoutEmailTest.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Test to check if a consignment without E-mail address is being submitted
+ *
+ * If you want to add improvements, please create a fork in our GitHub:
+ * https://github.com/myparcelnl
+ *
+ * @author      Reindert Vetter <reindert@myparcel.nl>
+ * @copyright   2010-2017 MyParcel
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US  CC BY-NC-ND 3.0 NL
+ * @link        https://github.com/myparcelnl/sdk
+ * @since       File available since Release v0.1.0
+ */
+
+namespace myparcelnl\sdk\Tests\SendConsignments;
+
+
+use MyParcelNL\Sdk\src\Helper\MyParcelCollection;
+use MyParcelNL\Sdk\src\Model\Repository\MyParcelConsignmentRepository;
+
+/**
+ * Class SendOneConsignmentWithoutEmailTest
+ * @package MyParcelNL\Sdk\tests\SendOneConsignmentWithoutEmailTest
+ */
+class SendOneConsignmentWithoutEmailTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test one shipment with createConcepts()
+     */
+    public function testSendOneConsignment()
+    {
+        foreach ($this->additionProvider() as $consignmentTest) {
+
+            $myParcelCollection = new MyParcelCollection();
+
+            $consignment = (new MyParcelConsignmentRepository())
+                ->setApiKey($consignmentTest['api_key'])
+                ->setCountry($consignmentTest['cc'])
+                ->setPerson($consignmentTest['person'])
+                ->setCompany($consignmentTest['company'])
+                ->setFullStreet($consignmentTest['full_street'])
+                ->setPostalCode($consignmentTest['postal_code'])
+                ->setCity($consignmentTest['city'])
+                ->setPhone($consignmentTest['phone'])
+                ->setPackageType($consignmentTest['package_type'])
+                ;
+
+            if (key_exists('label_description', $consignmentTest)) {
+                            $consignment->setLabelDescription($consignmentTest['label_description']);
+            }
+
+            $myParcelCollection->addConsignment($consignment);
+
+            /**
+             * Create concept
+             */
+            $myParcelCollection->createConcepts();
+
+            $this->assertEquals(true, $consignment->getMyParcelConsignmentId() > 1, 'No id found');
+            $this->assertEquals($consignmentTest['api_key'], $consignment->getApiKey(), 'getApiKey()');
+            $this->assertEquals($consignmentTest['cc'], $consignment->getCountry(), 'getCountry()');
+            $this->assertEquals($consignmentTest['person'], $consignment->getPerson(), 'getPerson()');
+            $this->assertEquals($consignmentTest['company'], $consignment->getCompany(), 'getCompany()');
+            $this->assertEquals($consignmentTest['full_street'], $consignment->getFullStreet(), 'getFullStreet()');
+            $this->assertEquals($consignmentTest['postal_code'], $consignment->getPostalCode(), 'getPostalCode()');
+            $this->assertEquals($consignmentTest['city'], $consignment->getCity(), 'getCity()');
+            $this->assertEquals($consignmentTest['phone'], $consignment->getPhone(), 'getPhone()');
+
+            if (key_exists('label_description', $consignmentTest)) {
+                            $this->assertEquals($consignmentTest['label_description'], $consignment->getLabelDescription(), 'getLabelDescription()');
+            }
+        }
+    }
+
+    /**
+     * Data for the test
+     * @return array
+     */
+    public function additionProvider()
+    {
+        return [
+            [
+                'api_key' => '94a98610ab9bf67a82873196c9ca688c601c179a',
+                'cc' => 'NL',
+                'person' => 'Piet',
+                'company' => 'Mega Store',
+                'full_street_test' => 'Koestraat 55',
+                'full_street' => 'Koestraat 55',
+                'street' => 'Koestraat',
+                'number' => 55,
+                'number_suffix' => '',
+                'postal_code' => '2231JE',
+                'city' => 'Katwijk',
+                'phone' => '123-45-235-435',
+                'package_type' => 1,
+                'large_format' => false,
+                'only_recipient' => false,
+                'signature' => false,
+                'return' => false,
+                'label_description' => 'Label description',
+            ],
+        ];
+    }
+}
