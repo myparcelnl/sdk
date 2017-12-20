254a770650b934faf95b7158b6122c6645b64a7f
Redouan Hamoudi <redouan@altyd.nl>
2017-12-20 14:42:52 +0100

    Fixed setting the pick up fields. Data being returned by the api no longer uses pickup_postal_code but postal_code instead.

--- src/Model/Repository/MyParcelConsignmentRepository.php
+++ src/Model/Repository/MyParcelConsignmentRepository.php
@@ -637,24 +637,24 @@ class MyParcelConsignmentRepository extends MyParcelConsignment
         // Set pickup
         if (key_exists('pickup', $data) && $data['pickup'] !== null) {
             $pickup = $data['pickup'];
-            if (key_exists('pickup_postal_code', $data['pickup'])) {
-                $this->setPickupPostalCode($pickup['pickup_postal_code']);
+            if (key_exists('postal_code', $pickup)) {
+                $this->setPickupPostalCode($pickup['postal_code']);
             }
 
-            if (key_exists('pickup_street', $pickup)) {
-                $this->setPickupStreet($pickup['pickup_street']);
+            if (key_exists('street', $pickup)) {
+                $this->setPickupStreet($pickup['street']);
             }
 
-            if (key_exists('pickup_city', $pickup)) {
-                $this->setPickupCity($pickup['pickup_city']);
+            if (key_exists('city', $pickup)) {
+                $this->setPickupCity($pickup['city']);
             }
 
-            if (key_exists('pickup_number', $pickup)) {
-                $this->setPickupNumber($pickup['pickup_number']);
+            if (key_exists('number', $pickup)) {
+                $this->setPickupNumber($pickup['number']);
             }
 
-            if (key_exists('pickup_location_name', $pickup)) {
-                $this->setPickupLocationName($pickup['pickup_location_name']);
+            if (key_exists('location_name', $pickup)) {
+                $this->setPickupLocationName($pickup['location_name']);
             }
         } else {
             $this
