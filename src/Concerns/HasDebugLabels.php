78b568f0eecd6e3d420574fa92a380530f377090
Richard Perdaan <richard@myparcel.nl>
2019-06-25 15:17:03 +0200

    add return

--- src/Concerns/HasDebugLabels.php
+++ src/Concerns/HasDebugLabels.php
@@ -10,13 +10,18 @@ trait HasDebugLabels
 {
     /**
      * @param $myParcelCollection
+     * @param $massage
+     *
+     * @return void
      */
-    public function debugLinkOfLabels($myParcelCollection, $massage)
+    public function debugLinkOfLabels($myParcelCollection, $massage): void
     {
         if (! getenv('CI')) {
             echo "\033[32mGenerated " . $massage . ": \033[0m";
             print_r($myParcelCollection->getLinkOfLabels());
             echo "\n\033[0m";
         }
+
+        return;
     }
 }
