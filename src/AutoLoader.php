ddcc8892fbda442a851cf9dde9c37f1d1815620a
Richard Perdaan <richard@myparcel.nl>
2019-10-17 10:34:33 +0200

    add enter

--- src/AutoLoader.php
+++ src/AutoLoader.php
@@ -15,12 +15,14 @@ foreach (getPhpFiles() as $phpFile) {
 function getPhpFiles(string $dir = '.'): array
 {
     $root = scandir($dir);
+
     // Loop in all folders and select the PHP files
     foreach($root as $value)
     {
         if ($value === '.' || $value === '..') {
             continue;
         }
+
         // Find if there is a php file in the directory
         if (is_file("$dir/$value")) {
             if (strpos($value, '.php')) {
@@ -28,6 +30,7 @@ function getPhpFiles(string $dir = '.'): array
             }
             continue;
         }
+        
         // find PHP files in sub-folders  with a recursive foreach
         foreach(getPhpFiles("$dir/$value") as $path)
         {
