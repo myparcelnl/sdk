375040a1e75c2b341025d0cde17174cad5ca5b1b
Richard Perdaan <richard@myparcel.nl>
2019-10-17 10:30:41 +0200

    find and select all php files in project

--- src/AutoLoader.php
+++ src/AutoLoader.php
@@ -1,40 +1,40 @@
-<?php declare(strict_types=1);
-require_once('Support/CollectionProxy.php');
-require_once('Support/Arr.php');
-require_once('Support/Collection.php');
-require_once('Support/Helpers.php');
-require_once('Support/HigherOrderCollectionProxy.php');
-require_once('Support/Str.php');
-require_once('Helper/RequestError.php');
-require_once('Helper/MyParcelCollection.php');
-require_once('Helper/MyParcelCurl.php');
-require_once('Helper/SplitStreet.php');
-require_once('Helper/LabelHelper.php');
-require_once('Helper/CheckoutFields.php');
-require_once('Services/CheckApiKeyService.php');
-require_once('Services/ConsignmentEncode.php');
-require_once('Services/CollectionEncode.php');
-require_once('Concerns/HasCheckoutFields.php');
-require_once('Model/MyParcelRequest.php');
-require_once('Adapter/ConsignmentAdapter.php');
-require_once('Adapter/DeliveryOptions/AbstractDeliveryOptionsAdapter.php');
-require_once('Adapter/DeliveryOptions/AbstractPickupLocationAdapter.php');
-require_once('Adapter/DeliveryOptions/AbstractShipmentOptionsAdapter.php');
-require_once('Adapter/DeliveryOptions/DeliveryOptionsV3Adapter.php');
-require_once('Adapter/DeliveryOptions/PickupLocationV3Adapter.php');
-require_once('Adapter/DeliveryOptions/ShipmentOptionsV3Adapter.php');
-require_once('Adapter/DeliveryOptions/DeliveryOptionsV2Adapter.php');
-require_once('Adapter/DeliveryOptions/PickupLocationV2Adapter.php');
-require_once('Adapter/DeliveryOptions/ShipmentOptionsV2Adapter.php');
-require_once('Model/MyParcelConsignment.php');
-require_once('Model/Consignment/AbstractConsignment.php');
-require_once('Model/Consignment/BpostConsignment.php');
-require_once('Model/Consignment/DPDConsignment.php');
-require_once('Model/Consignment/PostNLConsignment.php');
-require_once('Model/MyParcelCustomsItem.php');
-require_once('Model/FullStreet.php');
-require_once('Factory/ConsignmentFactory.php');
-require_once('Factory/DeliveryOptionsAdapterFactory.php');
-require_once('Exception/InvalidConsignmentException.php');
-require_once('Exception/ApiException.php');
-require_once('Exception/MissingFieldException.php');
+<?php
+
+declare(strict_types=1);
+
+// Require all php files
+foreach (getPhpFiles() as $phpFile) {
+    require_once($phpFile);
+}
+
+/**
+ * @param string $dir
+ *
+ * @return array
+ */
+function getPhpFiles(string $dir = '.'): array
+{
+    $root = scandir($dir);
+    // Loop in all folders and select the PHP files
+    foreach($root as $value)
+    {
+        if ($value === '.' || $value === '..') {
+            continue;
+        }
+        // Find if there is a php file in the directory
+        if (is_file("$dir/$value")) {
+            if (strpos($value, '.php')) {
+                $result[]="$dir/$value";
+            }
+            continue;
+        }
+        // find PHP files in sub-folders  with a recursive foreach
+        foreach(getPhpFiles("$dir/$value") as $path)
+        {
+            $result[] = $path;
+        }
+    }
+    return $result;
+}
+
+
