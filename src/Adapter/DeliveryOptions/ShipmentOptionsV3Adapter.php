922a2163be7605eae98e013b30e7e1aab0b754cf
Richard Perdaan <richard@myparcel.nl>
2019-11-27 15:54:22 +0100

    signature and only_recipient must be booleans

--- src/Adapter/DeliveryOptions/ShipmentOptionsV3Adapter.php
+++ src/Adapter/DeliveryOptions/ShipmentOptionsV3Adapter.php
@@ -5,14 +5,14 @@ namespace MyParcelNL\Sdk\src\Adapter\DeliveryOptions;
 class ShipmentOptionsV3Adapter extends AbstractShipmentOptionsAdapter
 {
     private const DEFAULT_INSURANCE = 0;
-    
+
     /**
      * @param array $shipmentOptions
      */
     public function __construct(array $shipmentOptions)
     {
-        $this->signature      = $shipmentOptions["signature"] ?? null;
-        $this->only_recipient = $shipmentOptions["only_recipient"] ?? null;
+        $this->signature      = $shipmentOptions["signature"] ?? false;
+        $this->only_recipient = $shipmentOptions["only_recipient"] ?? false;
         $this->insurance      = $shipmentOptions["insurance"] ?? self::DEFAULT_INSURANCE;
     }
 }
