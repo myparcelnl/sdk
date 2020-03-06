32aadde4ab36d04a4d026a3aa9c4ba5af1de57c5
Edie Lemoine <edie@myparcel.nl>
2020-03-06 10:54:06 +0100

    normalize postal code by removing any spaces in TrackTraceUrl

--- src/Helper/TrackTraceUrl.php
+++ src/Helper/TrackTraceUrl.php
@@ -7,6 +7,9 @@ class TrackTraceUrl
     const CONSUMER_PORTAL_BASE_URL = "https://myparcel.me/track-trace/";
 
     /**
+     * Creates a Track & Trace URL to myparcel.me from given barcode, postalCode and optional countryCode. Trims
+     * spaces from postal code and only adds country code to the url if it's added.
+     *
      * @param string      $barcode
      * @param string      $postalCode
      * @param string|null $countryCode
@@ -18,6 +21,8 @@ class TrackTraceUrl
         string $postalCode,
         ?string $countryCode = null
     ): string {
+        $postalCode = str_replace(' ', '', $postalCode);
+
         $url = self::CONSUMER_PORTAL_BASE_URL . "$barcode/$postalCode";
 
         if ($countryCode) {
