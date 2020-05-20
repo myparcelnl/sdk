68b03e2f12d0f6f91d21fa42de7c7f2024b8be43
Artem <artem.yakymchuk@gettonline.com>
2020-05-20 16:53:31 +0000

    Apply suggestion to src/Logger/Logger.php

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -9,7 +9,7 @@ class Logger
     public static function log($message, bool $is_exception = false)
     {
         if ($is_exception || \Configuration::get(Constant::MY_PARCEL_API_LOGGING_CONFIGURATION_NAME)) {
-            \PrestaShopLogger::addLog('[MYPARCEL]' . $message);
+            \PrestaShopLogger::addLog('[MYPARCEL] ' . $message);
         }
     }
 }
