a39eeb1fd278a3c9b4a18aaf7a8cbcebf7086d6f
Richard Perdaan <richard@myparcel.nl>
2020-07-21 13:18:05 +0200

    Update SendReturnLabelTest.php

--- Tests/ReturnLabelTest/SendReturnLabelTest.php
+++ Tests/ReturnLabelTest/SendReturnLabelTest.php
@@ -39,55 +39,72 @@ class SendReturnLabelTest extends \PHPUnit\Framework\TestCase
             return $this;
         }
 
-        $consignmentTest = $this->additionProviderNewConsignment();
+        foreach ($this->additionProviderNewConsignment() as $consignmentTest) {
+            $myParcelCollection = new MyParcelCollection();
 
-        $myParcelCollection = new MyParcelCollection();
+            $consignment = (ConsignmentFactory::createByCarrierId($consignmentTest['carrier_id']))
+                ->setApiKey($consignmentTest['api_key'])
+                ->setCountry($consignmentTest['cc'])
+                ->setPerson($consignmentTest['person'])
+                ->setCompany($consignmentTest['company'])
+                ->setFullStreet($consignmentTest['full_street_input'])
+                ->setPostalCode($consignmentTest['postal_code'])
+                ->setCity($consignmentTest['city'])
+                ->setEmail($consignmentTest['email'])
+                ->setPhone($consignmentTest['phone'])
+                ->setPackageType($consignmentTest['package_type'])
+                ->setLabelDescription($consignmentTest['label_description']);
 
-        $consignment = (ConsignmentFactory::createByCarrierId($consignmentTest['carrier_id']))
-            ->setApiKey($consignmentTest['api_key'])
-            ->setCountry($consignmentTest['cc'])
-            ->setPerson($consignmentTest['person'])
-            ->setCompany($consignmentTest['company'])
-            ->setFullStreet($consignmentTest['full_street_input'])
-            ->setPostalCode($consignmentTest['postal_code'])
-            ->setCity($consignmentTest['city'])
-            ->setEmail($consignmentTest['email'])
-            ->setPhone($consignmentTest['phone'])
-            ->setPackageType($consignmentTest['package_type'])
-            ->setLabelDescription($consignmentTest['label_description'])
-            ->setOnlyRecipient($consignmentTest['only_recipient'])
-            ->setSignature($consignmentTest['signature'])
-            ->setAgeCheck($consignmentTest['age_check'])
-            ->setReturn($consignmentTest['return'])
-            ->setInsurance($consignmentTest['insurance']);
+            if (key_exists('only_recipient', $consignmentTest)) {
+                $consignment->setOnlyRecipient($consignmentTest['only_recipient']);
+            }
 
-        $myParcelCollection
-            ->addConsignment($consignment)
-            ->generateReturnConsignments(
-                false,
-                function (
-                    AbstractConsignment $returnConsignment,
-                    AbstractConsignment $parent
-                ): AbstractConsignment {
-                    $returnConsignment->setLabelDescription(
-                        'Retour: ' . $parent->getLabelDescription() .
-                        ' This label is valid untill: ' . date("d-m-Y", strtotime("+ 21 day"))
-                    );
-                    $returnConsignment->setOnlyRecipient($parent->isOnlyRecipient());
-                    $returnConsignment->setSignature($parent->isSignature());
-                    $returnConsignment->setAgeCheck($parent->hasAgeCheck());
-                    $returnConsignment->setReturn($parent->isReturn());
-                    $returnConsignment->setLargeFormat($parent->isLargeFormat());
-                    $returnConsignment->setInsurance($parent->getInsurance());
+            if (key_exists('signature', $consignmentTest)) {
+                $consignment->setSignature($consignmentTest['signature']);
+            }
 
-                    return $returnConsignment;
-                }
-            )
-            ->setLinkOfLabels();
+            if (key_exists('age_check', $consignmentTest)) {
+                $consignment->setAgeCheck($consignmentTest['age_check']);
+            }
 
-        var_dump($myParcelCollection->getLinkOfLabels());
-        exit("\n|-------------\n" . __FILE__ . ':' . __LINE__ . "\n|-------------\n");
-        $this->assertContains('myparcel.nl/pdfs', $myParcelCollection->getLinkOfLabels());
+            if (key_exists('return', $consignmentTest)) {
+                $consignment->setReturn($consignmentTest['return']);
+            }
+
+            if (key_exists('large_format', $consignmentTest)) {
+                $consignment->setLargeFormat($consignmentTest['large_format']);
+            }
+
+            if (key_exists('insurance', $consignmentTest)) {
+                $consignment->setInsurance($consignmentTest['insurance']);
+            }
+
+            $myParcelCollection
+                ->addConsignment($consignment)
+                ->generateReturnConsignments(
+                    false,
+                    function (
+                        AbstractConsignment $returnConsignment,
+                        AbstractConsignment $parent
+                    ): AbstractConsignment {
+                        $returnConsignment->setLabelDescription(
+                            'Retour: ' . $parent->getLabelDescription() .
+                            ' This label is valid untill: ' . date("d-m-Y", strtotime("+ 21 day"))
+                        );
+                        $returnConsignment->setOnlyRecipient($parent->isOnlyRecipient());
+                        $returnConsignment->setSignature($parent->isSignature());
+                        $returnConsignment->setAgeCheck($parent->hasAgeCheck());
+                        $returnConsignment->setReturn($parent->isReturn());
+                        $returnConsignment->setLargeFormat($parent->isLargeFormat());
+                        $returnConsignment->setInsurance($parent->getInsurance());
+
+                        return $returnConsignment;
+                    }
+                )
+                ->setLinkOfLabels();
+
+            $this->assertContains('myparcel.nl/pdfs', $myParcelCollection->getLinkOfLabels());
+        }
     }
 
     /**
@@ -98,24 +115,27 @@ class SendReturnLabelTest extends \PHPUnit\Framework\TestCase
     private function additionProviderNewConsignment()
     {
         return [
-            'api_key'           => getenv('API_KEY'),
-            'carrier_id'        => PostNLConsignment::CARRIER_ID,
-            'cc'                => 'NL',
-            'person'            => 'Piet',
-            'email'             => 'richard@myparcel.nl',
-            'company'           => 'Mega Store',
-            'full_street_input' => 'Koestraat 55',
-            'number_suffix'     => '',
-            'postal_code'       => '2231JE',
-            'city'              => 'Katwijk',
-            'phone'             => '123-45-235-435',
-            'package_type'      => 1,
-            'label_description' => '1234',
-            'only_recipient'    => true,
-            'signature'         => true,
-            'age_check'         => true,
-            'return'            => true,
-            'insurance'         => 250
+            [
+                'api_key'           => getenv('API_KEY'),
+                'carrier_id'        => PostNLConsignment::CARRIER_ID,
+                'cc'                => 'NL',
+                'person'            => 'Piet',
+                'email'             => 'richard@myparcel.nl',
+                'company'           => 'Mega Store',
+                'full_street_input' => 'Koestraat 55',
+                'number_suffix'     => '',
+                'postal_code'       => '2231JE',
+                'city'              => 'Katwijk',
+                'phone'             => '123-45-235-435',
+                'package_type'      => 1,
+                'label_description' => '1234',
+                'only_recipient'    => true,
+                'signature'         => true,
+                'age_check'         => true,
+                'return'            => true,
+                'large_format'      => true,
+                'insurance'         => 250
+            ]
         ];
     }
 }
