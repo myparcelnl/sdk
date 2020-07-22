1ab84d0df366362aae136317840bcf70129b6c15
Richard Perdaan <richard@myparcel.nl>
2020-07-22 12:04:01 +0200

    replace  2010-2017 MyParcel

--- src/Model/MyParcelCustomsItem.php
+++ src/Model/MyParcelCustomsItem.php
@@ -6,7 +6,7 @@
  * https://github.com/myparcelnl
  *
  * @author      Reindert Vetter <reindert@myparcel.nl>
- * @copyright   2010-2017 MyParcel
+ * @copyright   2010-2020 MyParcel
  * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US  CC BY-NC-ND 3.0 NL
  * @link        https://github.com/myparcelnl/sdk
  * @since       File available since Release v0.1.0
