31b1732747544f06ad6523cb01561e5434bfbd29
Thijs Simonis <thijs.simonis@gettonline.com>
2021-05-21 12:06:27 +0000

    v1.0.7

--- myparcelbe.php
+++ myparcelbe.php
@@ -102,7 +102,7 @@ class MyParcelBE extends CarrierModule
     {
         $this->name = 'myparcelbe';
         $this->tab = 'shipping_logistics';
-        $this->version = '1.0.6';
+        $this->version = '1.0.7';
         $this->author = 'Gett';
         $this->need_instance = 1;
         $this->bootstrap = true;
