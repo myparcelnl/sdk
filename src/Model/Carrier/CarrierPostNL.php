af5ee395aca6291df5b545807a4538b87e410a56
Edie Lemoine <edie@myparcel.nl>
2021-10-26 16:17:23 +0200

    feat: add consignmentClass property/const to carrier classes

--- src/Model/Carrier/CarrierPostNL.php
+++ src/Model/Carrier/CarrierPostNL.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Sdk\src\Model\Carrier;
 
+use MyParcelNL\Sdk\src\Model\Consignment\PostNLConsignment;
+
 class CarrierPostNL extends AbstractCarrier
 {
-    public const HUMAN = 'PostNL';
-    public const ID    = 1;
-    public const NAME  = 'postnl';
+    public const CONSIGNMENT = PostNLConsignment::class;
+    public const HUMAN       = 'PostNL';
+    public const ID          = 1;
+    public const NAME        = 'postnl';
+
+    /**
+     * @var class-string
+     */
+    protected $consignmentClass = self::CONSIGNMENT;
 
     /**
      * @var string
