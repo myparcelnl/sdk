95c551da85bde7b5e0bc23d048e2668701ecc5ec
Edie Lemoine <edie@myparcel.nl>
2021-10-26 16:17:23 +0200

    feat: add consignmentClass property/const to carrier classes

--- src/Factory/ConsignmentFactory.php
+++ src/Factory/ConsignmentFactory.php
@@ -5,26 +5,11 @@ declare(strict_types=1);
 namespace MyParcelNL\Sdk\src\Factory;
 
 use MyParcelNL\Sdk\src\Model\Carrier\AbstractCarrier;
-use MyParcelNL\Sdk\src\Model\Carrier\CarrierBpost;
-use MyParcelNL\Sdk\src\Model\Carrier\CarrierDPD;
 use MyParcelNL\Sdk\src\Model\Carrier\CarrierFactory;
-use MyParcelNL\Sdk\src\Model\Carrier\CarrierPostNL;
-use MyParcelNL\Sdk\src\Model\Carrier\CarrierRedJePakketje;
 use MyParcelNL\Sdk\src\Model\Consignment\AbstractConsignment;
-use MyParcelNL\Sdk\src\Model\Consignment\BpostConsignment;
-use MyParcelNL\Sdk\src\Model\Consignment\DPDConsignment;
-use MyParcelNL\Sdk\src\Model\Consignment\PostNLConsignment;
-use MyParcelNL\Sdk\src\Model\Consignment\RedJePakketjeConsignment;
 
 class ConsignmentFactory
 {
-    private const CARRIER_CONSIGNMENT_MAP = [
-        CarrierBpost::class         => BpostConsignment::class,
-        CarrierDPD::class           => DPDConsignment::class,
-        CarrierPostNL::class        => PostNLConsignment::class,
-        CarrierRedJePakketje::class => RedJePakketjeConsignment::class,
-    ];
-
     /**
      * @param  int $carrierId
      *
@@ -68,8 +53,7 @@ class ConsignmentFactory
      */
     private static function getConsignmentFromCarrier(AbstractCarrier $carrier): AbstractConsignment
     {
-        $carrierClass     = get_class($carrier);
-        $consignmentClass = self::CARRIER_CONSIGNMENT_MAP[$carrierClass];
+        $consignmentClass = $carrier->getConsignmentClass();
 
         return new $consignmentClass();
     }
