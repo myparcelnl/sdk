a8ef2bbcca796935428cf314ea3b7a4de251c7b0
markernst97 <markiervc@gmail.com>
2022-04-21 13:27:42 +0200

    feat: add weight validation to consignment

---
+++ src/Rule/Consignment/MaximumWeightRule.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Sdk\src\Rule\Consignment;
+
+use Exception;
+use MyParcelNL\Sdk\src\Model\Consignment\AbstractConsignment;
+use MyParcelNL\Sdk\src\Rule\Rule;
+
+class MaximumWeightRule extends Rule
+{
+    /**
+     * @var int
+     */
+    public const MAX_COLLO_WEIGHT_GRAMS = 30000;
+
+    /**
+     * @param  \MyParcelNL\Sdk\src\Model\Consignment\AbstractConsignment $validationSubject
+     *
+     * @throws \Exception
+     */
+    public function validate($validationSubject): void
+    {
+        $weight = $validationSubject->getTotalWeight();
+
+        if (self::MAX_COLLO_WEIGHT_GRAMS < $weight) {
+            throw new Exception(
+                sprintf(
+                    'Order has not been exported to the MyParcel Backoffice. Shipment contains a weight of %s grams, maximum allowed is %s. Change package type or add more labels to distribute weight.',
+                    $weight,
+                    self::MAX_COLLO_WEIGHT_GRAMS
+                )
+            );
+        }
+    }
+}
