6c49e345f5e700b68391e409900a1874b9dc72a9
Thijmen Wijers <thijmen.wijers@myparcel.nl>
2022-07-05 10:48:45 +0200

    feat: check for empty values in array

--- src/Helper/Utils.php
+++ src/Helper/Utils.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Sdk\src\Helper;
 
+use MyParcelNL\Sdk\src\Support\Arr;
 use MyParcelNL\Sdk\src\Support\Str;
 
 class Utils
@@ -26,5 +27,23 @@ class Utils
                 $object->{$setter}($value);
             }
         }
+    }
+
+    /**
+     * @param $array
+     * @param $requiredKeys
+     *
+     * @return array
+     */
+    public static function emptyValues($array, $requiredKeys): array
+    {
+        $emptyValues = Arr::where(
+            $array,
+            static function ($value, $key) use ($requiredKeys) {
+                return (empty($value) && in_array($key, $requiredKeys, true));
+            }
+        );
+
+        return array_keys($emptyValues);
     }
 }
