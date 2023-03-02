eba62ae9c379012902fe2f7a547ed8f7efe518f1
joerivanveen <joeri@myparcel.nl>
2023-03-02 17:22:15 +0100

    fix: add valid insurance amounts for nl to be for postnl

--- src/Model/Consignment/PostNLConsignment.php
+++ src/Model/Consignment/PostNLConsignment.php
@@ -155,7 +155,18 @@ class PostNLConsignment extends AbstractConsignment
     protected function getNlToBeInsurancePossibilities(): array
     {
         return [
+            100,
+            250,
             500,
+            1000,
+            1500,
+            2000,
+            2500,
+            3000,
+            3500,
+            4000,
+            4500,
+            5000,
         ];
     }
 }
