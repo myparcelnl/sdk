85a7cbed1069eaf769fe3f2a348bd1cf3ca1a802
markernst97 <markiervc@gmail.com>
2023-04-04 10:08:24 +0200

    fix: tests

--- test/Services/DropOffPointServiceTest.php
+++ test/Services/DropOffPointServiceTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace MyParcelNL\Sdk\Test\Services;
 
 use Exception;
-use MyParcelNL\Sdk\src\Model\Carrier\CarrierInstabox;
+use MyParcelNL\Sdk\src\Model\Carrier\CarrierPostNL;
 use MyParcelNL\Sdk\src\Services\Web\DropOffPointWebService;
 use MyParcelNL\Sdk\Test\Bootstrap\TestCase;
 
@@ -19,7 +19,7 @@ class DropOffPointServiceTest extends TestCase
      */
     public function testGetDropOffPoint(): void
     {
-        $service = (new DropOffPointWebService(new CarrierInstabox()))->setApiKey($this->getApiKey());
+        $service = (new DropOffPointWebService(new CarrierPostNL()))->setApiKey($this->getApiKey());
         $result  = $service->getDropOffPoint('e9149b66-7bee-439b-bab0-7a5d92ddc519');
 
         if ($result) {
@@ -37,7 +37,7 @@ class DropOffPointServiceTest extends TestCase
      */
     public function testGetDropOffPoints(): void
     {
-        $service       = (new DropOffPointWebService(new CarrierInstabox()))->setApiKey($this->getApiKey());
+        $service       = (new DropOffPointWebService(new CarrierPostNL()))->setApiKey($this->getApiKey());
         $dropOffPoints = $service->getDropOffPoints('6825ME');
 
         self::assertNotEmpty($dropOffPoints->all(), 'No dropoff points found');
