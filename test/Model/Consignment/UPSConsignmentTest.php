be6d2d544ab4a9ee1dd16b29468fb6c6fb8fece7
Thijmen Wijers <thijmen.wijers@myparcel.nl>
2023-11-13 13:21:44 +0100

    fix: test

--- test/Model/Consignment/UPSConsignmentTest.php
+++ test/Model/Consignment/UPSConsignmentTest.php
@@ -16,9 +16,7 @@ class UPSConsignmentTest extends ConsignmentTestCase
     public function provideUPSConsignmentsData(): array
     {
         return $this->createConsignmentProviderDataset([
-            'NL -> NL' => [
-                'package_type' => 1,
-            ],
+            'NL -> NL' => $this->getDefaultAddress(),
         ]);
     }
 
