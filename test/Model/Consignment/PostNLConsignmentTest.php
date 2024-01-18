3be3816da587896a206497d0066e44f075c81e5e
joerivanveen <joeri@myparcel.nl>
2024-01-18 10:19:51 +0100

    test: fix insurance no longer mandatory towards belgium

--- test/Model/Consignment/PostNLConsignmentTest.php
+++ test/Model/Consignment/PostNLConsignmentTest.php
@@ -19,9 +19,9 @@ class PostNLConsignmentTest extends ConsignmentTestCase
         return $this->createConsignmentProviderDataset([
             'NL -> NL' => [],
             'NL -> BE' => $this->getDefaultAddress(AbstractConsignment::CC_BE) + [
-                    self::expected(self::INSURANCE) => 500,
-                    self::expected(self::ONLY_RECIPIENT) => true,
-                    self::expected(self::SIGNATURE)      => true,
+                    self::expected(self::INSURANCE) => 0,
+                    self::expected(self::ONLY_RECIPIENT) => false,
+                    self::expected(self::SIGNATURE)      => false,
                 ],
             'BE -> BE' => $this->getDefaultAddress(AbstractConsignment::CC_BE) + [
                     self::API_KEY => $this->getApiKey(self::ENV_API_KEY_BE),
