a72bfc46306babaffdf62901ef96d6d56a6498e9
Joeri van Veen <joeri@myparcel.nl>
2024-04-16 14:52:08 +0200

    fix: shipmentOptions and pickupLocation should be snake_case for backwards compatibility

--- src/Adapter/DeliveryOptions/PickupLocationV3Adapter.php
+++ src/Adapter/DeliveryOptions/PickupLocationV3Adapter.php
@@ -9,13 +9,13 @@ class PickupLocationV3Adapter extends AbstractPickupLocationAdapter
      */
     public function __construct(array $data)
     {
-        $this->location_name     = $data["location_name"];
-        $this->location_code     = $data["location_code"];
-        $this->retail_network_id = $data["retail_network_id"] ?? '';
-        $this->street            = $data["street"];
-        $this->number            = $data["number"];
-        $this->postal_code       = $data["postal_code"];
-        $this->city              = $data["city"];
-        $this->cc                = $data["cc"];
+        $this->location_name     = $data['location_name'];
+        $this->location_code     = $data['location_code'];
+        $this->retail_network_id = $data['retail_network_id'] ?? '';
+        $this->street            = $data['street'];
+        $this->number            = $data['number'];
+        $this->postal_code       = $data['postal_code'];
+        $this->city              = $data['city'];
+        $this->cc                = $data['cc'];
     }
 }
--- src/Adapter/DeliveryOptions/ShipmentOptionsV3Adapter.php
+++ src/Adapter/DeliveryOptions/ShipmentOptionsV3Adapter.php
@@ -10,7 +10,6 @@ class ShipmentOptionsV3Adapter extends AbstractShipmentOptionsAdapter
     public function __construct(array $shipmentOptions)
     {
         $this->age_check         = $shipmentOptions['age_check'] ?? null;
-        $this->extra_assurance   = $shipmentOptions['extra_assurance'] ?? null;
         $this->hide_sender       = $shipmentOptions['hide_sender'] ?? null;
         $this->insurance         = $shipmentOptions['insurance'] ?? null;
         $this->label_description = $shipmentOptions['label_description'] ?? null;
