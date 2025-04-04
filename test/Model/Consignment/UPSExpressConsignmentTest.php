a6d36073f3421a40fc5b75ae589d856ab4d4f50c
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-04-04 13:50:10 +0200

    feat: add tests

---
+++ test/Model/Consignment/UPSExpressConsignmentTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Sdk\Test\Model\Consignment;
+
+use MyParcelNL\Sdk\Model\Carrier\CarrierUPSExpressSaver;
+use MyParcelNL\Sdk\Model\Consignment\AbstractConsignment;
+use MyParcelNL\Sdk\Test\Bootstrap\ConsignmentTestCase;
+
+class UPSExpressConsignmentTest extends ConsignmentTestCase
+{
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function provideUPSExpressConsignmentsData(): array
+    {
+        return $this->createConsignmentProviderDataset([
+            'NL -> EU' => [],
+            'NL -> NL' => [
+                self::COUNTRY => AbstractConsignment::CC_NL,
+                self::POSTAL_CODE => '1234AB',
+                self::CITY => 'Amsterdam',
+                self::FULL_STREET => 'Hoofdstraat 1',
+            ],
+        ]);
+    }
+
+    /**
+     * @param  array $testData
+     *
+     * @throws \Exception
+     * @dataProvider provideUPSExpressConsignmentsData
+     */
+    public function testUPSExpressConsignments(array $testData): void
+    {
+        $this->doConsignmentTest($testData);
+    }
+
+    /**
+     * @return array|string[]
+     * @throws \Exception
+     */
+    protected function getDefaultConsignmentData(): array
+    {
+        return array_replace(
+            parent::getDefaultConsignmentData(),
+            [
+                self::CARRIER_ID   => CarrierUPSExpressSaver::ID,
+                self::PACKAGE_TYPE => AbstractConsignment::PACKAGE_TYPE_PACKAGE,
+                self::FULL_STREET  => 'Feldstrasse 17',
+                self::POSTAL_CODE  => '39394',
+                self::CITY         => 'Schwanebeck',
+                self::COUNTRY      => 'DE',
+                self::PHONE        => '123456',
+            ]
+        );
+    }
+} 
---
+++ test/Model/Consignment/UPSStandardConsignmentTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Sdk\Test\Model\Consignment;
+
+use MyParcelNL\Sdk\Model\Carrier\CarrierUPSStandard;
+use MyParcelNL\Sdk\Model\Consignment\AbstractConsignment;
+use MyParcelNL\Sdk\Test\Bootstrap\ConsignmentTestCase;
+
+class UPSStandardConsignmentTest extends ConsignmentTestCase
+{
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function provideUPSStandardConsignmentsData(): array
+    {
+        return $this->createConsignmentProviderDataset([
+            'NL -> EU' => [],
+            'NL -> NL' => [
+                self::COUNTRY => AbstractConsignment::CC_NL,
+                self::POSTAL_CODE => '1234AB',
+                self::CITY => 'Amsterdam',
+                self::FULL_STREET => 'Hoofdstraat 1',
+            ],
+        ]);
+    }
+
+    /**
+     * @param  array $testData
+     *
+     * @throws \Exception
+     * @dataProvider provideUPSStandardConsignmentsData
+     */
+    public function testUPSStandardConsignments(array $testData): void
+    {
+        $this->doConsignmentTest($testData);
+    }
+
+    /**
+     * @return array|string[]
+     * @throws \Exception
+     */
+    protected function getDefaultConsignmentData(): array
+    {
+        return array_replace(
+            parent::getDefaultConsignmentData(),
+            [
+                self::CARRIER_ID   => CarrierUPSStandard::ID,
+                self::PACKAGE_TYPE => AbstractConsignment::PACKAGE_TYPE_PACKAGE,
+                self::FULL_STREET  => 'Feldstrasse 17',
+                self::POSTAL_CODE  => '39394',
+                self::CITY         => 'Schwanebeck',
+                self::COUNTRY      => 'DE',
+                self::PHONE        => '123456',
+            ]
+        );
+    }
+} 
