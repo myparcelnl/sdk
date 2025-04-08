9950b77d99f80c52e717cc931dcd56762ca56789
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-04-08 13:37:00 +0200

    Add new lines to new files

--- test/Model/Consignment/UPSExpressConsignmentTest.php
+++ test/Model/Consignment/UPSExpressConsignmentTest.php
@@ -75,4 +75,4 @@ class UPSExpressConsignmentTest extends ConsignmentTestCase
             ]
         );
     }
-} 
+}
--- test/Model/Consignment/UPSStandardConsignmentTest.php
+++ test/Model/Consignment/UPSStandardConsignmentTest.php
@@ -75,4 +75,4 @@ class UPSStandardConsignmentTest extends ConsignmentTestCase
             ]
         );
     }
-} 
+}
