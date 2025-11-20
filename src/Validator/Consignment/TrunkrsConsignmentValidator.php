ef40ad97ecb36fc5e251813967c8fd755ca6eada
Joeri van Veen <joeri@myparcel.nl>
2025-11-20 16:45:48 +0100

    style: remove unused use statement

--- src/Validator/Consignment/TrunkrsConsignmentValidator.php
+++ src/Validator/Consignment/TrunkrsConsignmentValidator.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Sdk\Validator\Consignment;
 
-use MyParcelNL\Sdk\Rule\Consignment\MinimumWeightRule;
 use MyParcelNL\Sdk\Rule\Consignment\ShipmentOptionsRule;
 use MyParcelNL\Sdk\Validator\AbstractValidator;
 
