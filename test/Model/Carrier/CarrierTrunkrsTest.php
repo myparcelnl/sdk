227dd172683bc1f44722d6a24c778ac0f66ad3f0
Nabil Azahaf <nabil.azahaf@myparcel.nl>
2026-03-31 16:06:16 +0200

    refactor: remove consignment validators, rules and test fix
    Remove all carrier-specific consignment validators and consignment
    rules. These were only used by the now-deleted consignment models.
    Also remove leftover consignment class reference from CarrierTrunkrsTest.

--- test/Model/Carrier/CarrierTrunkrsTest.php
+++ test/Model/Carrier/CarrierTrunkrsTest.php
@@ -6,7 +6,6 @@ namespace MyParcelNL\Sdk\Test\Model\Carrier;
 
 use MyParcelNL\Sdk\Model\Carrier\CarrierFactory;
 use MyParcelNL\Sdk\Model\Carrier\CarrierTrunkrs;
-use MyParcelNL\Sdk\Model\Consignment\TrunkrsConsignment;
 use MyParcelNL\Sdk\Test\Bootstrap\TestCase;
 
 class CarrierTrunkrsTest extends TestCase
@@ -39,15 +38,5 @@ class CarrierTrunkrsTest extends TestCase
         $this->assertEquals(16, $carrier->getId());
     }
     
-    /**
-     * Test that Trunkrs carrier has correct consignment class
-     *
-     * @throws \Exception
-     */
-    public function testConsignmentClass(): void
-    {
-        $carrier = CarrierFactory::createFromId(CarrierTrunkrs::ID);
-        
-        $this->assertEquals(TrunkrsConsignment::class, $carrier->getConsignmentClass());
-    }
+
 }
